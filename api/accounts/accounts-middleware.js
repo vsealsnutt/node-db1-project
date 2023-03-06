dc8ae06a05fe712ae8cc8699294806da4f7c0d16
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-06 11:36:08 -0500

    corrected errors in middleware

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,13 +1,13 @@
 const db = require('../../data/db-config');
+const Account = require('./accounts-model');
 
 function checkAccountPayload(req, res, next) {
   // DO YOUR MAGIC
   // Note: you can either write "manual" validation logic
   // or use the Yup library (not currently installed)
   const { name, budget } = req.body;
-  if (!name || !budget) {
+  if (name === undefined || budget === undefined) {
     next({ status: 400, message: 'name and budget are required' });
-    next();
   } else if (name.trim().length < 3 || name.trim().length > 100) {
     next({ status: 400, message: 'name of account must be between 3 and 100' });
   } else if (typeof budget !== 'number') {
@@ -21,21 +21,30 @@ function checkAccountPayload(req, res, next) {
 
 async function checkAccountNameUnique(req, res, next) {
   // DO YOUR MAGIC
-  const existingName = await db('accounts').where('name', req.body.name.trim());
-  if (existingName) {
-    next({ status: 400, message: 'that name is taken' });
-  } else {
-    next();
+  try {
+    const existingName = await db('accounts').where('name', req.body.name.trim());
+    if (existingName) {
+      next({ status: 400, message: 'that name is taken' });
+    } else {
+      next();
+    }
+  } catch (err) {
+    next(err);
   }
 };
 
 async function checkAccountId(req, res, next) {
   // DO YOUR MAGIC
-  const account = await db('accounts').where('id', req.params.id).first();
-  if (!account) {
-    next({ status: 404, message: 'account not found' });
-  } else {
-    next();
+  try {
+    const account = await Account.getById(req.params.id);
+    if (!account) {
+      next({ status: 404, message: 'account not found' });
+    } else {
+      req.account = account;
+      next();
+    }
+  } catch (err) {
+    next(err);
   }
 };
 
