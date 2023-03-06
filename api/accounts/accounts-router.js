9e467027fe26358cf8f1e36d5d912678aace3318
Vanessa Seals-Nutt <v.sealsnutt@gmail.com>
2023-03-05 19:26:12 -0500

    wrote the get function to get all accounts

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,60 @@
-const router = require('express').Router()
+const express = require('express');
+const Account = require('./accounts-model');
+const { checkAccountPayload, checkAccountNameUnique, checkAccountId } = require('./accounts-middleware');
 
-router.get('/', (req, res, next) => {
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+    const accounts = await Account.getAll();
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get('/:id', (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+
+  } catch (err) {
+    
+  }
+});
 
 router.post('/', (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+
+  } catch (err) {
+    
+  }
+});
 
 router.put('/:id', (req, res, next) => {
   // DO YOUR MAGIC
+  try {
+
+  } catch (err) {
+    
+  }
 });
 
 router.delete('/:id', (req, res, next) => {
   // DO YOUR MAGIC
-})
+  try {
+
+  } catch (err) {
+    
+  }
+});
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
-})
+  res.status(err.status || 500).json({
+    message: err.message,
+  });
+});
 
 module.exports = router;
